7c7641d2b495d9fe4599a0a08041c9637eedbad7
Gilles Ochsenbein <gilles.ochsenbein@edifixio.com>
2013-06-11 15:04:58 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestApp::Application.config.secret_token = 'efea3e5332880c18af07a1249b8eb3ffe008b23623a94f1b7ff0a67fddfef3ce105d6741ae5456dbe3d703ffa594f4f6f3fb539043921d34077ddd82f52c1637'
